3c6706c5a085d9f7bc4ee99dff2969768beab7c7
rbhira <bhirarachid33@gmail.com>
2026-09-16 20:44:01 +0200

    feat(auth): add Angular authentication interface

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
+
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 import { Upload } from './features/upload/upload';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'register',
+    component: Register
+  },
+  {
+    path: 'upload',
     component: Upload
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
   }
 ];
